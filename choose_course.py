be96f8a1cb7d8758bc1cb5a5f380be801eb16eaf
sevenddddddd <sevenddddddd@gmail.com>
2018-01-13 01:00:17 +0800

    recode running time

--- choose_course.py
+++ choose_course.py
@@ -1,7 +1,17 @@
-import requests
-import json
-import hashlib
-import time
+import requests, json, hashlib, time
+
+
+#Get the total running time
+def time_spent(t):
+    delta = int(time.time() - t)
+    if delta < 60:
+        return '%s秒' % delta
+    if delta < 3600:
+        return '%s分钟%s秒' % (delta//60, delta%60)
+    if delta < 86400:
+        return '%s小时%s分钟' % (delta//3600, delta//60%60)
+    return '%s天%s小时' % (delta//86400, delta//3600%24)
+
 
 #Login via stuNum and password
 def login(session, j_username, j_password):
@@ -34,17 +44,25 @@ def login(session, j_username, j_password):
         return False
 
 
+#Repeatly send request to the server
 def start_fucking_the_server(session, j_username, j_password, kch, kxh):
+    print('[抢课已开始，关闭本程序将导致抢课中断]')
+    start_time = time.time()
     url = 'http://bkjwxk.sdu.edu.cn/b/xk/xs/add/%s/%s' % (kch, kxh)
     while True:
         response = session.post(url)
         response.encoding = 'utf-8'
+        if '缓存' in response.text:
+            print('[无效的课程号和课序号，请重新输入]')
+            kch = input('>>>请输入课程号 (eg:sd03031770)')
+            kxh = input('>>>请输入课序号 (eg:100)')
+            start_fucking_the_server(session, j_username, j_password, kch, kxh)
+            break
         if '成功' in response.text:
-            print('课程号：%s 课序号：%s 选课成功' % (kch, kxh))
+            print('[课程号：%s 课序号：%s 选课成功]' % (kch, kxh))
             break
         time.sleep(3)
-        print(response.text)
-
+        print('[程序已运行%s]' % time_spent(start_time), response.text)
 
 
 if __name__ == '__main__':
@@ -59,9 +77,9 @@ if __name__ == '__main__':
         j_username = input('>>>请输入你的学号\n')
         j_password = input('>>>请输入你的选课密码\n')
         login_succeeded = login(session, j_username, j_password)
-    print(session.cookies)
+    # print(session.cookies)
 
     print('>>>请自行登录选课网站，查询想要抢的课程的课程号和课序号。每次只能抢一门课哟！')
     kch = input('>>>请输入课程号 (eg:sd03031770)')
     kxh = input('>>>请输入课序号 (eg:100)')
-    start_fucking_the_server(session, j_username, j_password, 'sd03031770', '100')
+    start_fucking_the_server(session, j_username, j_password, kch, kxh)
